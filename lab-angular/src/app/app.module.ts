0a881adc9bb2d8e2338ce4a0d588c1c2adc867be
mssraposo <61809180+mssraposo@users.noreply.github.com>
2020-12-01 11:04:02 -0300

    formreativo

--- lab-angular/src/app/app.module.ts
+++ lab-angular/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { ContadorComponent } from './contador/contador.component';
 import { CardComponent } from './card/card.component';
+import { FormReativoComponent } from './form-reativo/form-reativo.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RodapeComponent,
     ContadorComponent,
-    CardComponent
+    CardComponent,
+    FormReativoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
